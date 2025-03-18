177bbb12fdb08515e42bce629285940783f82548
pranjalrai099 <pranjalkrai2004@gmail.com>
2025-03-18 23:01:32 +0530

    Delete models directory

--- models/Cart.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CartSchema = new mongoose.Schema({
-    place: { type: mongoose.Schema.Types.ObjectId, ref: 'Place', required: true },
-    user:{type: mongoose.Schema.Types.ObjectId,required:true},
-});
-const Cart = mongoose.model('Cart', CartSchema);
-module.exports = Cart;
